b34af2b525e409e6520cf0607d69882fdedaec50
Rafael <rafael.nava.1403@gmail.com>
2026-01-07 13:13:20 -0600

    feat(ApiKey): Creacion de ApiKey

--- src/graphql/resolvers/apiKey.resolvers.ts
+++ src/graphql/resolvers/apiKey.resolvers.ts
@@ -1,13 +1,20 @@
 import { ApiKeyService } from "../../services/apiKey.service";
 import { requireAuth } from "../../guards/auth.guard";
-import { createContext } from "node:vm";
 
 
 
 export const apiKeyResolvers = {
     ApiKey: {
-        createdAt: (parent: any) => parent.created_at,
-        updatedAt: (parent: any) => parent.updated_at,
+        // createdAt: (parent: any) => parent.created_at,
+        // updatedAt: (parent: any) => parent.updated_at,
+        userId: (parent: any) => {
+            // if (parent.user && parent.user.id) {
+            //     return parent.user.id;
+            // }
+            // Por si acaso TypeORM devuelve 'user_id' plano en algunas consultas raw
+            return parent.user_id || null; 
+        },
+        apiKey: (parent: any) => parent.api_key,
     },
     Query: {
         getApiKeyByUserId: async (_: any, context: any) => await ApiKeyService.getApiKeyByUserId(context.userId),
@@ -15,12 +22,15 @@ export const apiKeyResolvers = {
     },
     Mutation: {
         createApiKey: requireAuth(async (_: any, { input }: { input: any }, context: any) => {
+            input.userId = context.user.id;
             return await ApiKeyService.createApiKey(input)
         }),
         renewApiKey: requireAuth(async (_: any, { input }: { input: any }, context: any) => {
+            input.userId = context.user.id;
             return await ApiKeyService.renewApiKey(input.apiKey, input.type, input.userId)
         }),
         deleteLogicalApiKey: requireAuth(async (_: any, { input }: { input: any }, context: any) => {
+            input.userId = context.user.id;
             return await ApiKeyService.deleteLogicalApiKey(input.apiKey)
         }),
     }
--- src/services/apiKey.service.ts
+++ src/services/apiKey.service.ts
@@ -1,41 +1,41 @@
 import { ApiKey } from "../entities/ApiKey.entity";
-import { Repository } from "typeorm";
 import { v4 as uuidv4 } from 'uuid';
+import { AppDataSource } from "../config/typeorm.config";
 
 export class ApiKeyService {
 
-    private static keyRepository: Repository<ApiKey>;
+    private static apiKeyRepository = AppDataSource.getRepository(ApiKey);
 
 
     public static async createApiDevelopmentKey(userId: string) {
-        return await this.createApiKey({
-            userId: userId,
+        return await this.apiKeyRepository.create({
+            user: { id: userId },
             type: 'development',
             rate_limit: 10000,
-            rate_limit_per_day: 10000
+            rateLimitPerDay: 10000
         });
     }
 
     public static async createApiProductionKey(userId: string) {
-        return await this.createApiKey({
-            userId: userId,
+        return await this.apiKeyRepository.create({
+            user: { id: userId },
             type: 'production',
             rate_limit: 1000,
-            rate_limit_per_day: 1000
+            rateLimitPerDay: 1000
         });
     }
 
     static async deleteLogicalApiKey(apiKey: ApiKey) {
         apiKey.isActive = false;
-        await this.keyRepository.save(apiKey);
+        await this.apiKeyRepository.save(apiKey);
     }
 
     static async updateApiKey(apiKey: ApiKey) {
-        await this.keyRepository.save(apiKey);
+        await this.apiKeyRepository.save(apiKey);
     }
 
     static async validateApiKey(apiKey: string) {
-        const apiKeyEntity = await this.keyRepository.findOneBy({ api_key: apiKey });
+        const apiKeyEntity = await this.apiKeyRepository.findOneBy({ api_key: apiKey });
         if (apiKeyEntity?.isActive === false || apiKeyEntity?.isActive === null) {
             throw new Error('Invalid API key');
         }
@@ -43,23 +43,26 @@ export class ApiKeyService {
     }
 
     static async getApiKeys() {
-        return await this.keyRepository.find();
+        return await this.apiKeyRepository.find();
     }
 
     static async getApiKeyById(id: string) {
-        return await this.keyRepository.findOneBy({ id });
+        return await this.apiKeyRepository.findOneBy({ id });
     }
 
     static async getApiKeyByApiKey(apiKey: string) {
-        return await this.keyRepository.findOneBy({ api_key: apiKey });
+        return await this.apiKeyRepository.findOneBy({ api_key: apiKey });
     }
 
     static async getApiKeyByUserId(userId: string) {
-        return await this.keyRepository.findOneBy({ user: { id: userId } });
+        const apukeys = await this.apiKeyRepository.find({ 
+            where: { user: { id: userId } }
+        });
+        return apukeys;
     }
 
     static async getApiKeyActiveByType(type: 'development' | 'production') {
-        return await this.keyRepository.findOneBy({ type, isActive: true });
+        return await this.apiKeyRepository.find({ where: { type, isActive: true } });
     }
 
     static async renewApiKey(apiKey: string, type: 'development' | 'production', userId: string) {
@@ -84,18 +87,19 @@ export class ApiKeyService {
         rate_limit: number;
         rate_limit_per_day: number;
     }) {
+
         const { userId, type, rate_limit, rate_limit_per_day } = apiKeyData;
 
         const apiKeyCode = uuidv4();
 
-        const apiKey = this.keyRepository.create({
+        const apiKey = this.apiKeyRepository.create({
             api_key: apiKeyCode,
             user: { id: userId },
             type,
             rate_limit,
             rateLimitPerDay: rate_limit_per_day
         });
-        await this.keyRepository.save(apiKey);
+        await this.apiKeyRepository.save(apiKey);
         return apiKey;
     }
 
